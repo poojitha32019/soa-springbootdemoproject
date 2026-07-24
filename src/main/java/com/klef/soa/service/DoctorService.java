fd0ba7590a7b2c55ebc10007ef96aef61d87e951
poojitha32019 <vutlapallipoojitha@gmail.com>
2026-07-24 08:51:57 +0530

    V1.1

--- src/main/java/com/klef/soa/service/DoctorService.java
+++ src/main/java/com/klef/soa/service/DoctorService.java
@@ -1,14 +1,15 @@
 package com.klef.soa.service;
 
-
-
 import java.util.List;
-
 import com.klef.soa.entity.Doctor;
 
 public interface DoctorService {
 	Doctor addDoctor(Doctor d);
 	Doctor updateDoctor(Doctor d);
 	List<Doctor> displayAllDoctors();
+	Doctor displayDoctorById(Long id);
+	String deleteDoctorById(Long id);
+	List<Doctor> displayDoctorsByGender(String gender);
+	
 
 }
